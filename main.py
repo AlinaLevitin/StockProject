aa8f68afeefbddd84c302cfd706774e5693bae4b
alinag <alina.levitin123@gmail.com>
2022-07-14 16:28:15 +0300

    Merging probability and DL

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import probability
 import config
+import os
+import read_data_for_DL
 
-df = probability.open_csv('proc_table.csv')
-probability.get_winning_statistics(df, config.threshold, config.hits)
+cwd = os.getcwd()
+
+# df = probability.open_csv('proc_table.csv')
+# probability.get_winning_statistics(df, config.threshold, config.hits)
+
+read_data_for_DL.read_all_data(cwd)
 
 
 
