db274773c6797bf4f329bc94ceb41ea863dc2c7b
alinag <alina.levitin123@gmail.com>
2022-08-18 14:27:57 +0300

    Merge remote-tracking branch 'origin/master'

--- read_data.py
+++ read_data.py
@@ -7,5 +7,5 @@ cwd = os.getcwd()
 
 data = DeepLearnig.TrainingData(cwd)
 data.read_all_data(config.STEPS_BACK, config.STEPS_FORWARD, config.PERCENT, config.INTERVAL)
-
+data.save_all_data()
 print(data)
